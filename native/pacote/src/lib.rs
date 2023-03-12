ed55d7d1e66792d4047e4303fe7c969cdb517b4d
Marcos Soares <mavmaso@gmail.com>
2023-03-12 13:32:47 -0400

    add csv_reader/2 w/ tests

--- native/pacote/src/lib.rs
+++ native/pacote/src/lib.rs
@@ -1,6 +1,73 @@
+
+use std::collections::HashMap;
+use rustler::{Atom, Error, NifMap, NifResult, types::atom::ok};
+use csv::{ReaderBuilder};
+
+#[derive(NifMap)]
+pub struct CsvData{
+    placeholders: Vec<String>,
+    destination_count: i32,
+    example_data: HashMap<String, String>
+}
+
+fn valid_bit(s: &str) -> u8{
+    match s {
+        ";" => b';',
+        "|" => b'|',
+        "\t" => b'\t',
+        _ => b',',
+    }
+}
+
+
+#[rustler::nif]
+fn csv_reader(path: String, separator: String) ->  NifResult<(Atom, CsvData)>{
+    let mut file = ReaderBuilder::new()
+        .delimiter(valid_bit(&separator))
+        .from_path(&path)
+        .map_err(|_| Error::Atom("file_not_found"))?;
+
+    let placeholders = file.headers()
+        .map_err(|_| Error::Atom("no_header"))?
+        .iter().map(|s| s.to_string()).collect::<Vec<String>>();
+
+    // let example_data = match file.records().next() {
+    //     Some(Ok(record)) => {
+    //         let keys = placeholders.clone().into_iter();
+    //         let values = record.iter().map(|s| s.to_string()).collect::<Vec<String>>();
+    //         if keys.len() == values.len() {
+    //             keys.zip(values).collect()
+    //         } else {
+    //             return Err(Error::Atom("example_data_error"));
+    //         }
+    //     },
+    //     Some(Err(_)) => return Err(Error::Atom("failed_to_read_first_row")),
+    //     None => return Err(Error::Atom("no_rows_found")),
+    // };
+
+    let example_data = HashMap::new();
+
+    let mut destination_count = 0;
+    for row in file.records() {
+        match row {
+            Ok(_) => destination_count += 1,
+            Err(_) => return Err(Error::Atom("row_format")),
+        }
+    };
+
+    let csv_data = CsvData {
+        destination_count,
+        placeholders,
+        example_data,
+    };
+
+    Ok((ok(), csv_data))
+}
+
+
 #[rustler::nif]
 fn add(a: i64, b: i64) -> i64 {
     a + b
 }
 
-rustler::init!("Elixir.Pacote", [add]);
+rustler::init!("Elixir.Pacote", [add, csv_reader]);
--- test/pacote_test.exs
+++ test/pacote_test.exs
@@ -6,4 +6,11 @@ defmodule PacoteTest do
       assert Pacote.add(1, 2) == 3
     end
   end
+
+  describe "csv_reader/2" do
+    test "returns ok when valid data" do
+      path = "test/files/valid_a.csv"
+      assert {:ok, %{destination_count: 5}} = Pacote.csv_reader(path, ",")
+    end
+  end
 end
